20c10be6b8a2df5a8c8ab3af5b2b2519a3384544
andongni0723 <x102039@gmail.com>
2021-11-22 15:17:29 +0800

    new project '9x9.py'

--- python/9x9.py
+++ python/9x9.py
@@ -1,4 +1,9 @@
+#for i in range(1, 10):
+#    for j in range(1, 10):
+#            print("{}x{}={:2}" .format(i, j, i*j))
+#    print()
+    
 for i in range(1, 10):
     for j in range(1, 10):
-            print("{}x{}={}" .format(i, j, i*j))
+        print("{}x{}={:2}" .format(j, i, i*j) ,end=" ")
     print()
